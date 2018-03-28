ed53285dc73da27308dd4624048a011ddd29d6d4
OnlineAlex <becasualua@gmail.com>
2018-03-28 16:11:42 +0300

    написал рабочую версию скрипта / заполнил README

--- README.md
+++ README.md
@@ -1,7 +1,40 @@
-# Github Trends
+# Тренды Github
 
-[TODO. There will be project description]
+Скрипт выводит список ТОП-20 репозиториев недели на [github.com](https://github.com). Критерий: количество звезд.
+ Рейтинг включает количество открытых задач для каждого репозитория.
 
-# Project Goals
+### Пример
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+```bash
+> python github_trending.py
+  1: UnityCsReference               stars=2505   открыто issues=0    https://github.com/Unity-Technologies/UnityCsReference
+  2: GameNetworkingSockets          stars=1093   открыто issues=0    https://github.com/ValveSoftware/GameNetworkingSockets
+ ....
+ 19: figma-api-demo                 stars=102    открыто issues=2    https://github.com/figma/figma-api-demo
+ 20: person-blocker                 stars=91     открыто issues=2    https://github.com/minimaxir/person-blocker
+```
+# Требования
+Совестимые OC:
+* Linux,
+* Windows
+* MacOS
+
+Скрипт требует для своей работы установленного интерпретатора Python версии 3.5 выше
+
+И  пакетов из requirements.txt
+```bash
+pip install -r requirements.txt # или командой pip3
+```
+
+# Как работать
+> Запуск для всех ОС одинаковый
+Стандатной командой `python` (на некоторых компьютерах `python3`).
+```bash
+$ python github_trending.py
+```
+
+Помните, рекомендуется использовать [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) для лучшего управления пакетами.
+
+# Цели проекта
+
+Код создан в учебных целях. В рамках учебного курса по веб-разработке - [DEVMAN.org](https://devman.org)
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,63 @@
+import requests
+from datetime import datetime, timedelta
+
+API_GITHUB = 'https://api.github.com'
 
 
-def get_trending_repositories(top_size):
-    pass
+def get_date_for_git_api():
+    date_search = datetime.now() - timedelta(days=7)
+    date_for_git = datetime.strftime(date_search, '%Y-%m-%dT%H:%M')
+    return date_for_git
+
+
+def get_trending_repositories(date_start_search, top_size):
+    response = requests.get(
+        '{}/search/repositories?q=created:>{}&sort=star'.format(
+            API_GITHUB,
+            date_start_search
+        )
+    )
+
+    for item in range(top_size):
+        repository = response.json()['items'][item]
+        yield {
+            'repo_owner': repository['owner']['login'],
+            'repo_name': repository['name'],
+            'stars': repository['stargazers_count'],
+            'repo_url': repository['html_url']
+        }
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    pass
+    response = requests.get('{}/repos/{}/{}/issues'.format(
+        API_GITHUB,
+        repo_owner,
+        repo_name
+    ))
+    open_issues = response.json()
+
+    return len(open_issues)
+
 
 if __name__ == '__main__':
-    pass
+    date_start = get_date_for_git_api()
+    try:
+        trending_repositories = get_trending_repositories(
+            date_start,
+            top_size=20
+        )
+
+        for index, repo in enumerate(trending_repositories, start=1):
+            issues_amount = get_open_issues_amount(
+                repo['repo_owner'],
+                repo['repo_name']
+            )
+            print('{:>2}: {:<30} stars={:<4}   открыто issues={:<2}   {}'.format(
+                index,
+                repo['repo_name'],
+                repo['stars'],
+                issues_amount,
+                repo['repo_url']
+            ))
+    except requests.ConnectionError:
+        exit('Не удалось подключится к github.com')
